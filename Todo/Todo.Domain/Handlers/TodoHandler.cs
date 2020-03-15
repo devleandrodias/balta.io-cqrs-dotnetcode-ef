2f8d189d6ad127bc7f16bdf92e74fb21c2a95938
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-03-15 10:27:52 -0300

    Fazendo implementação do TodoHandler

---
+++ Todo/Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,31 @@
+using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Ropositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler :
+        Notifiable,
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
