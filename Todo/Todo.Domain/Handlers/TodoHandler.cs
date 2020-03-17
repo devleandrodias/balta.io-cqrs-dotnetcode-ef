ca126eb4c1dbb58d5ea453f714ba6e489e50b97f
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-03-17 19:45:09 -0300

    Finalizando o handler de TodoHandler

--- Todo/Todo.Domain/Handlers/TodoHandler.cs
+++ Todo/Todo.Domain/Handlers/TodoHandler.cs
@@ -11,7 +11,9 @@ namespace Todo.Domain.Handlers
     public class TodoHandler :
         Notifiable,
         IHandler<CreateTodoCommand>,
-        IHandler<UpdateTodoCommand>
+        IHandler<UpdateTodoCommand>,
+        IHandler<MarkTodoAsDoneCommand>,
+        IHandler<MarkTodoAsUndoneCommand>
     {
         private readonly ITodoRepository _repository;
 
@@ -57,6 +59,46 @@ namespace Todo.Domain.Handlers
 
             // Retorna o resultado
             return new GenericCommandResult(true, "Tarefa atualizada com sucesso!", command.Notifications);
+        }
+
+        public ICommandResult Handle(MarkTodoAsDoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada", command.Notifications);
+
+            // Recuperar o TodoItem
+            TodoItem todoItem = _repository.GetById(command.Id, command.User);
+
+            // Alterar o estado
+            todoItem.MarkAsDone();
+
+            // Salvar no banco
+            _repository.Update(todoItem);
+
+            // Retona o resultado 
+            return new GenericCommandResult(true, "Tarefa salva com sucesso", todoItem);
+        }
+
+        public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada", command.Notifications);
+
+            // Recuperar o TodoItem
+            TodoItem todoItem = _repository.GetById(command.Id, command.User);
+
+            // Alterar o estado
+            todoItem.MarkAsUndone();
+
+            // Salvar no banco
+            _repository.Update(todoItem);
+
+            // Retona o resultado 
+            return new GenericCommandResult(true, "Tarefa salva com sucesso", todoItem);
         }
     }
 }
