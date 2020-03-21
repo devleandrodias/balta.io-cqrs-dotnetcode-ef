6e92ea0108c8bec49678818b6b6c1a530c62b5e2
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-03-21 11:17:49 -0300

    Testando métodos GET e POST da API

--- Todo/Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo/Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,17 +1,27 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
 using Todo.Domain.Commands.Generics;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
+using Todo.Domain.Ropositories;
 
 namespace Todo.Domain.Api.Controllers
 {
     [Route("/v1/todos"), ApiController]
     public class TodoController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAll("devleandrodias");
+        }
+
         [HttpPost]
         public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
         {
-
+            command.User = "devleandrodias";
+            return (GenericCommandResult)handler.Handle(command);
         }
     }
 }
