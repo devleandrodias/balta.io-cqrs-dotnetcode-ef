40cadc05cdbd2bb3f024590f2f7209a6699b39d2
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-03-21 13:16:04 -0300

    Integração com banco de dados SQL Server

--- Todo/Todo.Domain.Api/Startup.cs
+++ Todo/Todo.Domain.Api/Startup.cs
@@ -23,7 +23,8 @@ namespace Todo.Domain.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("Database"));
+            // services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("connectionString")));
             services.AddTransient<ITodoRepository, TodoRepository>();
             services.AddTransient<TodoHandler, TodoHandler>();
         }
