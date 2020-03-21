c9781a085ecb8d87bcee42cefb7a5d5d90ba9643
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-03-21 14:01:11 -0300

    Conectando ao firebase autentificação

--- Todo/Todo.Domain.Api/Startup.cs
+++ Todo/Todo.Domain.Api/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Todo.Domain.Ropositories;
 using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Todo.Domain.Api
 {
@@ -23,10 +25,24 @@ namespace Todo.Domain.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            // services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("Database"));
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("connectionString")));
             services.AddTransient<ITodoRepository, TodoRepository>();
             services.AddTransient<TodoHandler, TodoHandler>();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://securetoken.google.com/dotnet-api-67799";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://securetoken.google.com/dotnet-api-67799",
+                        ValidateAudience = true,
+                        ValidAudience = "dotnet-api-67799",
+                        ValidateLifetime = true
+                    };
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
