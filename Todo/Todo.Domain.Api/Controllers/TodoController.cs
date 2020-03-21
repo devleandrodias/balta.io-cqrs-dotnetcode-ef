70479af11a2085e13b104718d0c044d5e8b9fbe2
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-03-21 11:39:46 -0300

    Finalizando o controller

--- Todo/Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo/Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
@@ -11,16 +12,76 @@ namespace Todo.Domain.Api.Controllers
     [Route("/v1/todos"), ApiController]
     public class TodoController : ControllerBase
     {
+        private string _userName = "devleandrodias";
+        private DateTime _today = DateTime.Now.Date;
+
         [HttpGet]
         public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
         {
-            return repository.GetAll("devleandrodias");
+            return repository.GetAll(_userName);
+        }
+
+        [HttpGet("done")]
+        public IEnumerable<TodoItem> GetAllDone([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllDone(_userName);
+        }
+
+        [HttpGet("done/today")]
+        public IEnumerable<TodoItem> GetDoneForToday([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllByPeriod(_userName, _today, true);
+        }
+
+        [HttpGet("done/tomorrow")]
+        public IEnumerable<TodoItem> GetDoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllByPeriod(_userName, _today.AddDays(1), true);
+        }
+
+        [HttpGet("undone")]
+        public IEnumerable<TodoItem> GetAllUndone([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllDone(_userName);
+        }
+
+        [HttpGet("undone/today")]
+        public IEnumerable<TodoItem> GetAllUndoneForToday([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllByPeriod(_userName, _today, false);
+        }
+
+        [HttpGet("undone/tomorrow")]
+        public IEnumerable<TodoItem> GetAllUndoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllByPeriod(_userName, _today.AddDays(1), false);
         }
 
         [HttpPost]
         public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
         {
-            command.User = "devleandrodias";
+            command.User = _userName;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [HttpPut]
+        public GenericCommandResult Update([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _userName;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [HttpPut("mark-as-done")]
+        public GenericCommandResult MarkAsDone([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _userName;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [HttpPut("mark-as-undone")]
+        public GenericCommandResult MarkAsUndone([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = _userName;
             return (GenericCommandResult)handler.Handle(command);
         }
     }
